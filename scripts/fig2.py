0ba5f6ff1564431346c4f64092cc46b4d95362e0
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-10-04 20:34:23 +0200

    Minor.

--- scripts/fig2.py
+++ scripts/fig2.py
@@ -5,5 +5,5 @@ Figure(
     "9cm", "6cm",
     Panel(SVG(snakemake.input.dag).scale(0.24), label("a")),
     Panel(SVG(snakemake.input.workflow).scale(0.45), label("b")).move(220, 0),
-    Grid(40, 40)
+    # Grid(40, 40)
 ).save(snakemake.output[0])
