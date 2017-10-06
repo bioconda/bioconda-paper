7b5434debadbf9c3b3003dcc497785f2561647ac
Ryan Dale <dalerr@niddk.nih.gov>
2017-10-06 12:09:09 -0400

    restrict ecosystems to recipes

--- scripts/plot-ecosystems.py
+++ scripts/plot-ecosystems.py
@@ -1,16 +1,22 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
+import glob
+import os
 
 import common
 
 plt.figure(figsize=(4,2))
 
-packages = pd.read_table(snakemake.input[0])
+packages = pd.read_table(snakemake.input.tsv)
 packages.loc[packages.ecosystem == 'Bioconductor', 'ecosystem'] = 'Bioconductor/R'
 packages.loc[packages.ecosystem == 'R', 'ecosystem'] = 'Bioconductor/R'
+recipes = set(map(os.path.basename, glob.glob('bioconda-recipes/recipes/*')))
 
-sns.countplot(x="ecosystem", data=packages)
+packages['has_current_recipe'] = packages['package'].isin(recipes)
+
+
+sns.countplot(x="ecosystem", data=packages[packages.has_current_recipe])
 plt.ylabel("count")
 plt.text(plt.xlim()[1], plt.ylim()[1], "total: {}".format(packages.shape[0]),
          horizontalalignment="right", verticalalignment="top")
