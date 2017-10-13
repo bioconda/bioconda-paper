5a829c38e16e746a64ad85cc5a9e3636ff3d42f4
Johannes Köster <johannes.koester@tu-dortmund.de>
2017-10-13 16:28:48 +0200

    Fix label.

--- scripts/fig2.py
+++ scripts/fig2.py
@@ -6,6 +6,6 @@ Figure(
     Panel(SVG(snakemake.input.dag).scale(0.24), label("a")),
     Panel(SVG(snakemake.input.workflow).scale(0.45), label("b")).move(220, 0),
     Panel(SVG(snakemake.input.comp), label("c")).move(310, 0),
-    Panel(SVG(snakemake.input.turnaround), label("c")).move(595, 0),
+    Panel(SVG(snakemake.input.turnaround), label("d")).move(595, 0),
     #Grid(40, 40)
 ).save(snakemake.output[0])
