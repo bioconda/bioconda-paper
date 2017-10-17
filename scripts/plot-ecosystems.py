a2f99172dbe2ce472b378763379b9f716ac7d196
Ryan Dale <dalerr@niddk.nih.gov>
2017-10-17 12:17:06 -0400

    plot-ecosystems tweaks

--- scripts/plot-ecosystems.py
+++ scripts/plot-ecosystems.py
@@ -18,25 +18,23 @@ recipes = set(map(os.path.basename, glob.glob('bioconda-recipes/recipes/*')))
 packages['has_current_recipe'] = packages['package'].isin(recipes)
 packages['not_bio_related'] = packages['package'].isin(not_bio_related)
 
-fig = plt.figure(figsize=(4,3))
+fig = plt.figure(figsize=(4, 3))
 ax = fig.add_subplot(1, 1, 1)
 all_cnts = packages.ecosystem.value_counts()
 bio_cnts = packages[~packages.not_bio_related].ecosystem.value_counts()
 non_cnts = packages[packages.not_bio_related].ecosystem.value_counts()
 
 x = range(len(all_cnts))
-ax.bar(x=x, height=bio_cnts, color=sns.color_palette('colorblind'))
-with sns.color_palette('Set1', desat=0.7) as c:
-    ax.bar(x=x, height=non_cnts, bottom=bio_cnts, color=sns.color_palette(sns.color_palette('colorblind'), desat=0.5))
-ax.set_ylabel('count')
+ax.bar(x=x, height=bio_cnts, color=sns.color_palette())
+ax.bar(x=x, height=non_cnts, bottom=bio_cnts, color=sns.color_palette(sns.color_palette(), desat=0.5))
+ax.set_ylabel('Available packages')
 ax.set_xticks(x)
 ax.set_xticklabels(list(all_cnts.index))
-
-#sns.countplot(x="ecosystem", data=packages)
-plt.ylabel("count")
-plt.text(plt.xlim()[1], plt.ylim()[1], "total: {}".format(packages.shape[0]),
-         horizontalalignment="right", verticalalignment="top")
+ax.set_ylabel("count")
+ax.text(x=0.5, y=1, s="Total packages: {}".format(packages.shape[0]),
+         horizontalalignment="center", verticalalignment="top",
+        transform=ax.transAxes)
 sns.despine()
-plt.gcf().tight_layout()
+fig.tight_layout()
 
 plt.savefig(snakemake.output[0], bbox_inches="tight")
