8b43238f7d7ce6a6a8479b474c786bad3e2b6ef8
Ryan Dale <dalerr@niddk.nih.gov>
2017-10-17 12:16:57 -0400

    plot-downloads tweaks

--- scripts/plot-downloads.py
+++ scripts/plot-downloads.py
@@ -15,7 +15,7 @@ packages.loc[packages.ecosystem == 'R', 'ecosystem'] = 'Bioconductor/R'
 
 # In case we want to filter downloads by whether or not a current recipe exists
 recipes = set(map(os.path.basename, glob.glob('bioconda-recipes/recipes/*')))
-sns.set_palette('colorblind')
+
 sns.boxplot(x="ecosystem",
             y="downloads",
             data=packages,
@@ -45,16 +45,24 @@ plt.savefig(snakemake.output[0], bbox_inches="tight")
 # Take the log10 ahead of time so the KDE works well.
 packages['log10 downloads'] = np.log10(packages.downloads + 1)
 
-plt.figure(figsize=(4,2))
+fig = plt.figure(figsize=(4, 3))
+ax = fig.add_subplot(1, 1, 1)
 sns.violinplot(
     x="ecosystem",
     y="log10 downloads",
     alpha=0.5,
     cut=0,
     data=packages,
+    ax=ax,
     order=['Bioconductor/R', 'Other', 'Python', 'Perl'],
 )
-plt.text(plt.xlim()[1], plt.ylim()[1], "total: {:,}".format(total_downloads),
-         horizontalalignment="right", verticalalignment="top")
+ax.text(x=0.5, y=1.0, s="Total downloads: {:,}".format(total_downloads),
+         horizontalalignment="center", verticalalignment="top",
+        transform=ax.transAxes)
+ax.set_xlabel('')
+
+# make a little room for the "total" text
+ax.axis(ymax=6)
+fig.tight_layout()
 sns.despine()
 plt.savefig(snakemake.output[1], bbox_inches="tight")
