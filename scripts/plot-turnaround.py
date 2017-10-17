81c098bc0e6f2f5cdad044de495ccf9bda22d5fd
Ryan Dale <dalerr@niddk.nih.gov>
2017-10-17 12:17:16 -0400

    plot turnaround tweaks

--- scripts/plot-turnaround.py
+++ scripts/plot-turnaround.py
@@ -5,7 +5,10 @@ import pandas as pd
 
 import common
 
-sns.set_palette("Blues")
+# Default palette was a little too dark for the text to show up in the last
+# block; increasing available colors lets us stay on the lighter side of the
+# palette.
+sns.set_palette("Greys", n_colors=8)
 
 prs = pd.read_table(snakemake.input[0])
 prs.span = pd.to_timedelta(prs.span)
@@ -20,11 +23,12 @@ binning = pd.cut(prs.span.dt.total_seconds(),
                  categories.dt.total_seconds(),
                  labels=labels)
 counts = binning.value_counts()
+
 # fix order
 counts = counts[labels]
 
 perc = counts / counts.sum()
-fig = plt.figure(figsize=(5, 1.3))
+fig = plt.figure(figsize=(5, 1.1))
 ax = fig.add_subplot(1, 1, 1)
 left = 0
 for label, x in perc.items():
@@ -33,7 +37,7 @@ for label, x in perc.items():
     left += x
 
 sns.despine(top=True, left=True, right=True, trim=True)
-ax.set_xlabel('Fraction of pull requests')
+ax.set_xlabel('Fraction of pull requests merged')
 ax.yaxis.set_visible(False)
 fig.tight_layout()
 fig.subplots_adjust(top=0.9)
