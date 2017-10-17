5102de4c6b06b710cb1cbc28af7bf51d0e78fbec
Ryan Dale <dalerr@niddk.nih.gov>
2017-10-17 12:17:25 -0400

    rearrange fig2

--- scripts/fig2.py
+++ scripts/fig2.py
@@ -2,10 +2,10 @@ from svgutils.compose import *
 from common import label
 
 Figure(
-    "28cm", "6cm",
+    "23.1cm", "6cm",
     Panel(SVG(snakemake.input.dag).scale(0.24), label("a")),
-    Panel(SVG(snakemake.input.workflow).scale(0.45), label("b")).move(220, 0),
-    Panel(SVG(snakemake.input.comp), label("c")).move(310, 0),
-    Panel(SVG(snakemake.input.turnaround), label("d")).move(595, 50),
+    Panel(SVG(snakemake.input.workflow).scale(0.5), label("b")).move(220, 0),
+    Panel(SVG(snakemake.input.turnaround).scale(0.9), label("c")).move(220, 50),
+    Panel(SVG(snakemake.input.comp).scale(0.8), label("d")).move(570, 0),
     #Grid(40, 40)
 ).save(snakemake.output[0])
